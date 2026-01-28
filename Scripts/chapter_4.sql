ad39e6dfffdc5404e9fedba06d398d862f57f214
Saurabh Varule <varulesaurabh44@gmail.com>
2026-01-28 22:35:01 +0530

    add chapter_4.sql

---
+++ Scripts/chapter_4.sql
@@ -0,0 +1,15 @@
+-- DML COMMANDS
+
+-- INSERT COMMAND (Already Done)
+
+-- UPDATE COMMAND
+UPDATE customers
+SET name = 'sam'
+WHERE email = 'aa';
+
+
+-- DELETE COMMAND
+DELETE FROM customers 
+WHERE email = 'aa';
+
+SELECT * FROM customers
